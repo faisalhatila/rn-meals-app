c4cfb35e547e3c73de209602c875ca5dad5dccdb
faisalhatila <hatilas1994@gmail.com>
2020-08-17 09:45:21 -0700

    on categories screen, navigation option used (not worked for mine should be asked to senior)

--- screens/categoriesscreen/index.js
+++ screens/categoriesscreen/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import { CATEGORIES } from "../../data/dummy-data";
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";
-
-const renderGridItem = (itemData) => {
-  return (
-    <View style={styles.gridItem}>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
-};
-
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
+import Colors from "../../constants/Colors";
 const CategoriesScreen = (props) => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity
+        style={styles.gridItem}
+        onPress={() => {
+          props.navigation.navigate({ routeName: "CategoryMeals" });
+        }}
+      >
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   return (
     <FlatList
       keyExtractor={(item, i) => item.id}
@@ -19,6 +33,14 @@ const CategoriesScreen = (props) => {
       numColumns={2}
     />
   );
+};
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "#fff" : Colors.primaryColor,
 };
 
 const styles = StyleSheet.create({
@@ -31,6 +53,8 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
