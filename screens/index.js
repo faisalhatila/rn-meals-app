a7f58213c16272d4de41bb139e2955293bd38575
faisalhatila <hatilas1994@gmail.com>
2020-08-17 07:43:32 -0700

    all screen exported

--- screens/index.js
+++ screens/index.js
@@ -1,7 +1,11 @@
-import _StartGameScreen from "./startgamescreen";
-import _GameScreen from "./gamescreen";
-import _GameOverScreen from "./gameoverscreen";
+import _CategoriesScreen from "./categoriesscreen";
+import _CategoryMealsScreen from "./categorymealsscreen";
+import _FavouritesScreen from "./favouritesscreen";
+import _FiltersScreen from "./filtersscreen";
+import _MealDetailScreen from "./mealdetailscreen";
 
-export const StartGameScreen = _StartGameScreen;
-export const GameScreen = _GameScreen;
-export const GameOverScreen = _GameOverScreen;
+export const CategoriesScreen = _CategoriesScreen;
+export const CategoryMealsScreen = _CategoryMealsScreen;
+export const FavouritesScreen = _FavouritesScreen;
+export const FiltersScreen = _FiltersScreen;
+export const MealDetailScreen = _MealDetailScreen;
