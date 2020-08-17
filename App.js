f7a8ca59323cbb2df6889f511310d5b8b06ee0c3
faisalhatila <hatilas1994@gmail.com>
2020-08-17 07:11:14 -0700

    fonts and loading component imported

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-
 import { View, Text, StyleSheet } from "react-native";
-
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
 const App = () => {
   return (
     <View style={styles.container}>
@@ -12,9 +12,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container: {
-    // flexDirection: "row",
-    width: "100%",
-    height: "100%",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
