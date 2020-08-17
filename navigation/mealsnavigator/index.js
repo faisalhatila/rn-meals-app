7c8eefd71d8a4e3f1a7a53a298165077d3c39193
faisalhatila <hatilas1994@gmail.com>
2020-08-17 10:16:32 -0700

    navigation options made dynamic

--- navigation/mealsnavigator/index.js
+++ navigation/mealsnavigator/index.js
@@ -1,14 +1,30 @@
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { Platform } from "react-native";
+import Colors from "../../constants/Colors";
 import {
   CategoriesScreen,
   CategoryMealsScreen,
   MealDetailScreen,
 } from "../../screens/index";
 const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
+  Categories: {
+    screen: CategoriesScreen,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "#fff" : Colors.primaryColor,
+    },
+  },
   CategoryMeals: {
     screen: CategoryMealsScreen,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "#fff" : Colors.primaryColor,
+    },
   },
   MealDetail: MealDetailScreen,
 });
