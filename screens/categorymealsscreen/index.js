e6dde2506ee2675ba3eb3cabad3e1ce83f75690e
faisalhatila <hatilas1994@gmail.com>
2020-08-17 09:54:33 -0700

    data passed to forward and fetched successfully

--- screens/categorymealsscreen/index.js
+++ screens/categorymealsscreen/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-
 import { View, Text, StyleSheet, Button } from "react-native";
-
+import { CATEGORIES } from "../../data/dummy-data";
 const CategoryMealsScreen = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return (
     <View>
       <Text>Category Meals Screen</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to details"
         onPress={() => {
