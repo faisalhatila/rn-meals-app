0d21485908b9b4f7b8deac798a014ff09f4e34df
faisalhatila <hatilas1994@gmail.com>
2020-08-17 07:15:46 -0700

    font loading async integerated

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-cold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 const App = () => {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading startAsync={fetchFonts} onFinish={setFontLoaded(true)} />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Meals App</Text>
