0134a3a19ed3c6d201b7c84dd48e403367caa57a
faisalhatila <hatilas1994@gmail.com>
2020-08-18 02:26:17 -0700

    header button component exported

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import _CategoryGridTile from "./categorygridtile.js";
 import _MealItem from "./mealitem";
+import _HeaderButton from "./headerbutton";
 
 export const CategoryGridTile = _CategoryGridTile;
 export const MealItem = _MealItem;
+export const HeaderButton = _HeaderButton;
