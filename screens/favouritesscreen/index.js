f83b0445d4a46ea0ad526eb7d4199ecf595abf7e
faisalhatila <hatilas1994@gmail.com>
2020-08-18 07:49:10 -0700

    side drawer added to favourites screen

--- screens/favouritesscreen/index.js
+++ screens/favouritesscreen/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import MealList from "../../components/meallist";
 import { MEALS } from "../../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { HeaderButton } from "../../components";
 const FavouritesScreen = (props) => {
   const favMeals = MEALS.filter((meal) => meal.id === "m1" || "m2");
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
-FavouritesScreen.navigationOptions = {
-  headerTitle: "Your Favourites",
+FavouritesScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Your Favourites",
+    headerLeft: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => {
+              navData.navigation.toggleDrawer();
+            }}
+          />
+        </HeaderButtons>
+      );
+    },
+  };
 };
 
 export default FavouritesScreen;
