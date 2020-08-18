017e663f7fad9bb52b36fc295cec388bb9960f37
faisalhatila <hatilas1994@gmail.com>
2020-08-18 02:00:56 -0700

    data passed to meal detail screen

--- screens/categorymealsscreen/index.js
+++ screens/categorymealsscreen/index.js
@@ -15,7 +15,14 @@ const CategoryMealsScreen = (props) => {
     return (
       <MealItem
         title={itemData.item.title}
-        onSelectMeal={() => {}}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
         duration={itemData.item.duration}
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
@@ -27,7 +34,6 @@ const CategoryMealsScreen = (props) => {
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
-  // const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
   return (
     <View style={styles.screen}>
@@ -45,10 +51,6 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
   return {
     headerTitle: selectedCategory.title,
-    // headerStyle: {
-    //   backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
-    // },
-    // headerTintColor: Platform.OS === "android" ? "#fff" : Colors.primaryColor,
   };
 };
 
