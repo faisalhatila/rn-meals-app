ec52a82e8cd1990b5ae4280414a076910cb85768
faisalhatila <hatilas1994@gmail.com>
2020-08-18 02:21:47 -0700

    header button component created

--- components/headerbutton/index.js
+++ components/headerbutton/index.js
@@ -5,9 +5,17 @@ import { HeaderButton } from "react-navigation-header-buttons";
 import { Ionicons } from "@expo/vector-icons";
 
 import Colors from "../../constants/Colors";
+import { Platform } from "react-native";
 
 const CustomHeaderButton = (props) => {
-  return <HeaderButton {...props} />;
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Ionicons}
+      iconSize={23}
+      color={Platform.OS === "android" ? "#fff" : Colors.primaryColor}
+    />
+  );
 };
 
 export default CustomHeaderButton;
