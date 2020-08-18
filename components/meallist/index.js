40f949f012aed339d89652719598769608e7fd59
faisalhatila <hatilas1994@gmail.com>
2020-08-18 06:39:14 -0700

    meal list component exported

--- components/meallist/index.js
+++ components/meallist/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 const MealList = (props) => {
-  return <FlatList />;
+  return (
+    <View style={styles.list}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+});
 
 export default MealList;
