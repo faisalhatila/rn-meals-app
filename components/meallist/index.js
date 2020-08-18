12802c8734815339e69ff81227ff870ad703b085
faisalhatila <hatilas1994@gmail.com>
2020-08-18 06:33:23 -0700

    meal list component created

---
+++ components/meallist/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { FlatList, StyleSheet } from "react-native";
+
+const MealList = (props) => {
+  return <FlatList />;
+};
+
+const styles = StyleSheet.create({});
+
+export default MealList;
