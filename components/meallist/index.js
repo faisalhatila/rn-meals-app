e5cd00e809d8a76bb53a072d57b7971fac270ef5
faisalhatila <hatilas1994@gmail.com>
2020-08-18 06:43:13 -0700

    meal list component created and integerated in category mean screen

--- components/meallist/index.js
+++ components/meallist/index.js
@@ -1,12 +1,31 @@
 import React from "react";
-
+import { MealItem } from "../../components";
 import { FlatList, StyleSheet, View } from "react-native";
 
 const MealList = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        image={itemData.item.imageUrl}
+      />
+    );
+  };
   return (
     <View style={styles.list}>
       <FlatList
-        data={displayedMeals}
+        data={props.listData}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
         style={{ width: "100%" }}
--- screens/categorymealsscreen/index.js
+++ screens/categorymealsscreen/index.js
@@ -1,50 +1,15 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  Platform,
-  FlatList,
-} from "react-native";
 import Colors from "../../constants/Colors";
 import { CATEGORIES, MEALS } from "../../data/dummy-data";
-import { MealItem } from "../../components";
+import { MealList } from "../../components";
+
 const CategoryMealsScreen = (props) => {
-  const renderMealItem = (itemData) => {
-    return (
-      <MealItem
-        title={itemData.item.title}
-        onSelectMeal={() => {
-          props.navigation.navigate({
-            routeName: "MealDetail",
-            params: {
-              mealId: itemData.item.id,
-            },
-          });
-        }}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        image={itemData.item.imageUrl}
-      />
-    );
-  };
   const catId = props.navigation.getParam("categoryId");
   const displayedMeals = MEALS.filter(
     (meal) => meal.categoryIds.indexOf(catId) >= 0
   );
 
-  return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item, index) => item.id}
-        renderItem={renderMealItem}
-        style={{ width: "100%" }}
-      />
-    </View>
-  );
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 CategoryMealsScreen.navigationOptions = (navigationData) => {
   const catId = navigationData.navigation.getParam("categoryId");
@@ -53,14 +18,5 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 10,
-  },
-});
 
 export default CategoryMealsScreen;
