ba870488744d405f584690368aeb011ded4e4751
faisalhatila <hatilas1994@gmail.com>
2020-08-18 06:42:37 -0700

    meal list component exported

--- components/index.js
+++ components/index.js
@@ -1,7 +1,9 @@
 import _CategoryGridTile from "./categorygridtile.js";
 import _MealItem from "./mealitem";
 import _HeaderButton from "./headerbutton";
+import _MealList from "./meallist";
 
 export const CategoryGridTile = _CategoryGridTile;
 export const MealItem = _MealItem;
 export const HeaderButton = _HeaderButton;
+export const MealList = _MealList;
