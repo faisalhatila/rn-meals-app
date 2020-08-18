b3a5e37736557cc2ebb3028b3ab0e8d3a7801220
faisalhatila <hatilas1994@gmail.com>
2020-08-18 01:33:07 -0700

    meal item component done

--- components/mealitem/index.js
+++ components/mealitem/index.js
@@ -1,16 +1,35 @@
 import React from "react";
 
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 
 const MealItem = (props) => {
   return (
     <View style={styles.mealItem}>
       <TouchableOpacity onPress={props.onSelectMeal}>
         <View>
-          <View style={styles.mealRow}>
-            <Text>{itemData.item.title}</Text>
+          <View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+            <ImageBackground
+              source={{ uri: props.image }}
+              style={styles.bgImage}
+            >
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {props.title}
+                </Text>
+              </View>
+            </ImageBackground>
           </View>
-          <View style={styles.mealRow}></View>
+          <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
+            <Text>{props.duration}m</Text>
+            <Text>{props.complexity.toUpperCase()}</Text>
+            <Text>{props.affordability.toUpperCase()}</Text>
+          </View>
         </View>
       </TouchableOpacity>
     </View>
@@ -19,12 +38,40 @@ const MealItem = (props) => {
 
 const styles = StyleSheet.create({
   mealItem: {
-    height: 200,
+    height: 250,
     width: "100%",
-    backgroundColor: "#ccc",
+    backgroundColor: "#f5f5f5",
+    marginVertical: 10,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    justifyContent: "flex-end",
   },
   mealRow: {
     flexDirection: "row",
+  },
+  mealHeader: {
+    height: "85%",
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.7)",
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+    color: "#fff",
+    textAlign: "center",
   },
 });
 
