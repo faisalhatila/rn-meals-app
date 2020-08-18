d70a5dcc37c1e189db27d757bcff76d7dc09af42
faisalhatila <hatilas1994@gmail.com>
2020-08-18 02:53:14 -0700

    start icon integerated

--- screens/mealdetailscreen/index.js
+++ screens/mealdetailscreen/index.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import { View, Text, StyleSheet, Button } from "react-native";
 import { MEALS } from "../../data/dummy-data";
-
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { HeaderButton } from "../../components";
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
@@ -25,6 +26,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favourite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("marked");
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
