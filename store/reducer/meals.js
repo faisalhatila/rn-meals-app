6248a1c8e8f8263baefa3fc1a8007487ec6a8fdc
faisalhatila <hatilas1994@gmail.com>
2020-08-19 05:39:17 -0700

    after calling action, reducer lifecycle done

--- store/reducer/meals.js
+++ store/reducer/meals.js
@@ -1,3 +1,5 @@
+import { TOGGLE_FAVOURITE } from "../actions/meals";
+
 const { MEALS } = require("../../../data/dummy-data");
 
 const initialState = {
@@ -7,6 +9,22 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_FAVOURITE:
+      const existingIndex = state.favourtieMeals.findIndex(
+        (meal) => meal.id === action.mealId
+      );
+      if (existingIndex >= 0) {
+        const updatedFavMeals = [...state.favourtieMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favourtieMeals: updatedFavMeals };
+      } else {
+        const meal = state.meals.find((meal) => meal.id === action.mealId);
+        return { ...state, favourtieMeals: state.favourtieMeals.concat(meal) };
+      }
+    default:
+      return state;
+  }
   return state;
 };
 
