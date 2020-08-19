86beee20d7f3e56b0c2eb289dc106e7f2321297f
faisalhatila <hatilas1994@gmail.com>
2020-08-19 00:10:35 -0700

    default text exported

--- components/index.js
+++ components/index.js
@@ -1,9 +1,9 @@
 import _CategoryGridTile from "./categorygridtile.js";
 import _MealItem from "./mealitem";
 import _HeaderButton from "./headerbutton";
-// import _MealList from "./meallist";
+import _DefaultText from "./defaulttext";
 
 export const CategoryGridTile = _CategoryGridTile;
 export const MealItem = _MealItem;
 export const HeaderButton = _HeaderButton;
-// export const MealList = _MealList;
+export const DefaultText = _DefaultText;
