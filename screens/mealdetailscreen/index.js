23aa8a84234b016cf1ad588fda2dbf81a9150644
faisalhatila <hatilas1994@gmail.com>
2020-08-19 05:23:58 -0700

    folder structure fixed

--- screens/mealdetailscreen/index.js
+++ screens/mealdetailscreen/index.js
@@ -1,14 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  ScrollView,
-  Image,
-} from "react-native";
-import { MEALS } from "../../data/dummy-data";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
+import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { HeaderButton, DefaultText } from "../../components";
 
@@ -21,8 +14,14 @@ const ListItem = (props) => {
 };
 
 const MealDetailScreen = (props) => {
+  const availableMeals = useSelector((state) => state.meals.meals);
   const mealId = props.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
+  useEffect(() => {
+    props.navigation.setParams({
+      mealTitle: selectedMeal.title,
+    });
+  }, [selectedMeal]);
   return (
     <ScrollView>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -45,9 +44,10 @@ const MealDetailScreen = (props) => {
 
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const mealTitle = navigationData.navigation.getParam("mealTitle");
+  // const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
