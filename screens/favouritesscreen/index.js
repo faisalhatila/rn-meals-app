15ca81bb2107ebaa5295a9223e7666e5d4aa05f6
faisalhatila <hatilas1994@gmail.com>
2020-08-19 07:39:02 -0700

    default text added when there is no fav meals

--- screens/favouritesscreen/index.js
+++ screens/favouritesscreen/index.js
@@ -4,8 +4,17 @@ import MealList from "../../components/meallist";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { HeaderButton } from "../../components";
+import { View, StyleSheet } from "react-native";
+import DefaultText from "../../components/defaulttext";
 const FavouritesScreen = (props) => {
   const favMeals = useSelector((state) => state.meals.favouriteMeals);
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No fav meals found, start adding some!!! </DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
 
@@ -27,5 +36,12 @@ FavouritesScreen.navigationOptions = (navData) => {
     },
   };
 };
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default FavouritesScreen;
